f23df586de38d5e2d95be52b988bf42e648662be
Gertjan <info@esolutions-it.nl>
2021-11-02 10:48:32 +0100

    Todo List form

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import App from './vue/app.vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+library.add(faPlusSquare, faTrash)
+
+const app = new Vue({
+    el: '#app',
+    components:{ App }
+});
